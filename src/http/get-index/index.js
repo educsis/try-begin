afef0d34aa13754d6e627993cf0b6e9d78ebd9cf
Edwin Orellana <educsis@gmail.com>
2020-02-24 13:43:34 -0600

    Update index.js

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -7,10 +7,10 @@ exports.handler = async function Index () {
     /**
      * Basic bio
      */
-    fullname: 'Your Name', // ←  Start by adding your name!
-    title: 'My personal site!',
-    occupation: 'Artist & Photographer',
-    location: 'West Glacier, MT',
+    fullname: 'Edwin Orellana', // ←  Start by adding your name!
+    title: 'Dev After 40',
+    occupation: 'Developer',
+    location: 'Guatemala',
     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.',
 
     /**
